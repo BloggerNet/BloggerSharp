f1d73f532540531696da82ffb4e8314df717d04a
meil <ics.sora.meil@gmail.com>
2018-09-17 19:15:57 +0900

    Update git_push.bat

--- git_push.bat
+++ git_push.bat
@@ -1,15 +1,10 @@
-echo set config
 git config --global user.email "azure-pipelines[bot]@users.noreply.github.com"
 git config --global user.name "azure-pipelines[bot]"
 
-echo add track
 git add .
 
-echo show diff
 git status
 
-echo commit
 git commit -m "Update Docs"
 
-echo push
-git -c http.extraheader="AUTHORIZATION: basic $(env:SYSTEM_ACCESSTOKEN)" push origin master 
+git -c http.extraheader="AUTHORIZATION: basic %SYSTEM_ACCESSTOKEN%" push origin master 
