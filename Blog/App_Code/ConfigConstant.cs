f8ed7d35df4923fb5b267056569fce85b11dfd37
meil <ics.sora.meil@gmail.com>
2018-09-17 16:52:23 +0900

    delete core module

---
+++ Blog/App_Code/ConfigConstant.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+public class ConfigConstant
+{
+    public const string Title = "Title";
+    public const string Description = "Description";
+    public const string CopyrightYear = "CopyrightYear";
+    public const string CopyrightOwner = "CopyrightOwner";
+}
