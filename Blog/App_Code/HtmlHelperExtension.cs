ed33ee1abc46d72c096970db740ca6a27c3d307c
meil <ics.sora.meil@gmail.com>
2018-09-17 12:25:05 +0900

    プロジェクト ファイルを追加します。

---
+++ Blog/App_Code/HtmlHelperExtension.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.WebPages.Html;
+
+public static class HtmlHelperExtension
+{
+    /// <summary>
+    /// Empty method, fix build error
+    /// </summary>
+    /// <param name="html"></param>
+    /// <param name="value"></param>
+    public static string Partial(this HtmlHelper html, string value)
+    {
+        return value;
+    }
+}
---
+++ Core/ConfigConstant.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Core
+{
+    public class ConfigConstant
+    {
+        public const string Title = "Title";
+        public const string Description = "Description";
+        public const string CopyrightYear = "CopyrightYear";
+        public const string CopyrightOwner = "CopyrightOwner";
+    }
+}
