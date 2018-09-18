ac2da4f90776edf3e52bd6685f9a888729ce91f7
meil <ics.sora.meil@gmail.com>
2018-09-18 23:01:01 +0900

    Update git_push.bat

--- git_push.bat
+++ git_push.bat
@@ -15,3 +15,26 @@ xcopy /e docs ghpages
 
 echo show directory
 dir ghpages
+
+echo cd to ghpages
+cd ghpages
+
+echo set email %GITHUB_EMAIL%
+git config --global user.email "%GITHUB_EMAIL%"
+
+echo set user %GITHUB_USER%
+git config --global user.name "%GITHUB_USER%"
+
+echo add track
+git add .
+
+echo show diff
+git status
+
+echo commit
+git commit -m "Update Docs"
+
+echo push
+git push -q https://%GITHUB_TOKEN%@github.com/%GITHUB_OWNER%/%GITHUB_REPOSITORY%.git gh-pages > NUL 2>&1 
+
+echo finish
